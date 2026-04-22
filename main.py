6e10419fc5072d229d6b83fa94b7d6ef118e1727
parsonej <parsonej@rose-hulman.edu>
2026-04-22 17:47:50 -0400

    Performence V0.01

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ while True:
 		break
 	if (dt > Dt):
 		time.sleep(dt - Dt)
-score, metrics = controlla.calculate_performance(histDX, histDY, dt)
+score, metrics = motion.calculate_performance(histDX, histDY, dt)
 print(f"Performance Score: {score:.2f}")
 print(f"Metrics: {metrics}")
 
